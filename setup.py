354d6e880a2a4c90231505ee99179ed3510ab34a
ShengRang <541628498@qq.com>
2017-06-04 09:51:05 +0800

    ignore MANIFEST

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ljudge',
     packages=['ljudge'],
     version='0.2',
-    description='python bindings for ljudge',
+    description='python bindings for quark-zju/ljudge',
     author='shengrang',
     author_email='shengrang0@gmail.com',
     url='https://github.com/NJUST-FishTeam/ljudge-py',
